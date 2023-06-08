4389b20069a8ddaf4cb1ec04b758e752cc3ecc60
pelikhan <jhalleux@microsoft.com>
2023-06-08 15:49:16 -0700

    use observables

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
-import { delay, millis } from "@devicescript/core"
 import { init, request } from "."
+import { startSHTC3 } from "@devicescript/drivers"
+import { ewma, auditTime } from "@devicescript/observables"
 
 // configure product UID and serial number
 await init()
 
-while (true) {
-    // send node.add request
-    const res = await request({ req: "note.add", body: { time: millis() } })
-    console.log(res)
-    // take a break
-    await delay(10000)
-}
+// mount sensors
+const { temperature } = await startSHTC3()
+
+// process temperature readings
+temperature.reading
+    .pipe(
+        // filter data
+        ewma(0.9),
+        // debounce every 30s
+        auditTime(30000)
+    )
+    .subscribe(async t => { // t is the filtered temperature
+        // upload reading as `node.add` request
+        await request({ req: "note.add", body: { t } })
+    })
